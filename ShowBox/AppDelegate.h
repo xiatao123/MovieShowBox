a3aa0e53c06cd46d3ad239abde2d372a88a1d86e
taoxia <taoxia@yahoo-inc.com>
2014-01-07 17:42:25 -0800

    Initial Commit

---
+++ ShowBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShowBox
+//
+//  Created by Tao Xia on 1/7/14.
+//  Copyright (c) 2014 Tao Xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShowBox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShowBox
+//
+//  Created by Tao Xia on 1/7/14.
+//  Copyright (c) 2014 Tao Xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
